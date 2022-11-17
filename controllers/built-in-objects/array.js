a797465eafee9faa41084b280f0dac24121bdb7e
OgutuJoseph <onyango.joseph@yahoo.com>
2022-11-17 11:00:39 +0300

    Built-in-objects--array--length[properties]

--- controllers/built-in-objects/array.js
+++ controllers/built-in-objects/array.js
@@ -43,11 +43,81 @@ const unscoppables = () => {
  */
 };
 
+/** 3. length */
+/** **3.1 desc** */
+const lengthDesc = (req, res) => {
+    // The value of the length property is a non-negative integer with a value less than 2^32
+    const listA = [1, 2, 3];
+    const listB = new Array(6);
+    const listC = new Array(4); 
+    // listC.length = 2 ** 32 // RangeError: Invalid array length (4294967296)
+    // const listD = new Array(-100) // RangeError: Invalid array length (Negative numbers are not allowed)
+
+    console.log('listA length: ', listA.length);
+    console.log('listB length: ', listB.length);
+    // console.log('listC length: ', listC.length);
+    // console.log('listD: ', listD)
+    
+
+    const arr = [1, 2];
+    // increasing length, extends array by adding empty slots, not undefined values
+    arr.length = 5
+    console.log('arr: ', arr);
+
+
+};
+/** **3.2 iterate over array** */
+const iterateArray = (req, res) => {
+    // iterate over, and double values in an array
+    const numbers = [1, 2, 3, 4, 5];
+    const length = numbers.length;
+    for (let i = 0; i < length; i++) {
+        numbers[i] *= 2;
+    };
+    res.status(200).json(numbers);
+};
+/** **3.2 shorten array** */
+const shortenArray = (req, res) => {
+    // shorten the array numbers to a length of 3 if the current length is greater than 3
+    const numbers = [1, 2, 3, 4, 5];
+
+    if (numbers.length > 3) {
+        numbers.length = 3;
+    };
+
+    console.log(numbers[3]); // undefined; the extra elements are deleted
+    res.status(200).json(numbers);
+};
+/** **3.3 create empty array of fixed length** */
+const fixedLengthArray = (req, res) => {
+    const numbers = [];
+    numbers.length = 3;
+    // numbers['1'] = 54;
+    // console.log('numbers: ', numbers)
+    res.status(200).json(numbers);
+};
+/** **3.3 array with non-writable length** */
+const nonWritableArray = (req, res) => {
+    "use strict";
+
+    const numbers = [1, 2, 3, 4, 5];
+    Object.defineProperty(numbers, "length", { writable: false });
+
+    // numbers[5] = 6; // TypeError: Cannot assign to read only property 'length' of object '[object Array]'
+    // numbers.push(5); // // TypeError: Cannot assign to read only property 'length' of object '[object Array]'
+    res.status(200).json(numbers);
+};
+
 
 /** ###############----############### array methods ###############----############### */
 
 
 module.exports = {
     species,
-    unscoppables
+    unscoppables,
+    lengthDesc,
+    iterateArray,
+    shortenArray,
+    fixedLengthArray,
+    nonWritableArray
 }
--- routes/built-in-objects/array.js
+++ routes/built-in-objects/array.js
@@ -1,9 +1,18 @@
 const router = require('express').Router();
 const { 
-    species
+    species,
+    lengthDesc,
+    iterateArray,
+    shortenArray,
+    fixedLengthArray,
+    nonWritableArray
 } = require('../../controllers/built-in-objects/array');
 
 router.get('/species', species);
-router.get('/test', );
+router.get('/lengthDesc', lengthDesc);
+router.get('/iterateArray', iterateArray);
+router.get('/shortenArray', shortenArray);
+router.get('/fixedLengthArray', fixedLengthArray);
+router.get('/nonWriatbleArray', nonWritableArray)
 
 module.exports = router;
