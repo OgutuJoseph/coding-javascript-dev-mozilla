5495ddcd521faa72d4810c75dcb3c8bfad67b42c
OgutuJoseph <onyango.joseph@yahoo.com>
2022-11-17 10:16:03 +0300

    Built-in-objects--array--unscoppables[properties]

--- controllers/built-in-objects/array.js
+++ controllers/built-in-objects/array.js
@@ -19,10 +19,35 @@ const species = (req, res) => {
     res.status(200).json(concatResult);
 };
 
+/** 2. [@@unscopables] */
+const unscoppables = () => {
+    /**
+        The default Array properties that are ignored for with statement-binding purposes are:
+        -at()
+        -copyWithin()
+        -entries()
+        -fill()
+        -find()
+        -findIndex()
+        -findLast()
+        -findLastindex()
+        -flat()
+        -flatMap()
+        -includes()
+        -keys()
+        -values()
+
+        Array.prototype[@@unscopables] is an empty object only containing all the above property names with the value true.
+        Its prototype is null, so Object.prototype properties like toString won't accidentally be made unscopable, and a toString() within the with statement will continue to be called on the array.
+    
+ */
+};
+
 
 /** ###############----############### array methods ###############----############### */
 
 
 module.exports = {
-    species
+    species,
+    unscoppables
 }
