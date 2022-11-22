14bfc8b8fd249e664787aa67d9f6c5489e541d5d
OgutuJoseph <onyango.joseph@yahoo.com>
2022-11-22 12:42:50 +0300

    Built-in-objects--array--entries()

--- routes/built-in-objects/array.js
+++ routes/built-in-objects/array.js
@@ -3,7 +3,8 @@ const { species, lengthDesc, iterateArray, shortenArray, fixedLengthArray, nonWr
 const { iterator } = require('../../controllers/built-in-objects/array/1.iterator');
 const { atDesc, atReturnLastValue, atCompareMethods, atOnNonArrays } = require('../../controllers/built-in-objects/array/2.at()');
 const { concatDesc, concatTwo, concatThree, concatValuesTo, concatNestedArrays, concatSpreadable, concatSparseArrays, concatNonArrays } = require('../../controllers/built-in-objects/array/3.concat()')
-const { copyWithinDesc, copyWithinUse, copyWithinOnSparse, copyWithinOnNonArrays } = require('../../controllers/built-in-objects/array/4.copyWithin()')
+const { copyWithinDesc, copyWithinUse, copyWithinOnSparse, copyWithinOnNonArrays } = require('../../controllers/built-in-objects/array/4.copyWithin()');
+const { entriesDesc, entriesWithIndexAndElement, entriesWithFor_Of_Loop, entriesWithSparseArrays, entriesWithNonArrays } = require('../../controllers/built-in-objects/array/entries()');
 
 /** properties */
 router.get('/species', species);
@@ -35,5 +36,11 @@ router.get('/copyWithinDesc', copyWithinDesc);
 router.get('/copyWithinUse', copyWithinUse);
 router.get('/copyWithinOnSparse', copyWithinOnSparse);
 router.get('/copyWithinOnNonArrays', copyWithinOnNonArrays);
+/** **entries()**  */
+router.get('/entriesDesc', entriesDesc);
+router.get('/entriesWithIndexAndElement', entriesWithIndexAndElement);
+router.get('/entriesWithFor_Of_Loop', entriesWithFor_Of_Loop);
+router.get('/entriesWithSparseArrays', entriesWithSparseArrays);
+router.get('/entriesWithNonArrays', entriesWithNonArrays);
 
 module.exports = router;
