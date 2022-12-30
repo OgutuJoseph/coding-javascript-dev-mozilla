8a1ccd0b9a17da87881abc7819f705a412fb2106
OgutuJoseph <onyango.joseph@yahoo.com>
2022-12-30 15:04:04 +0300

    Built-in-objects--array-groupToMap()

--- controllers/built-in-objects/array/18.groupToMap()--.js
+++ controllers/built-in-objects/array/18.groupToMap()--.js
@@ -1,23 +1,23 @@
 /** ********** array methods - 18. groupToMap() ********** */
 /**
-    Syntax
-    // Arrow function
-    groupToMap((element) => { ... })
-    groupToMap((element, index) => { ... })
-    groupToMap((element, index, array) => { ... })
+   Syntax
+   // Arrow function
+   groupToMap((element) => { ... })
+   groupToMap((element, index) => { ... })
+   groupToMap((element, index, array) => { ... })
 
-    // Callback function
-    groupToMap(callbackFn)
-    groupToMap(callbackFn, thisArg)
+   // Callback function
+   groupToMap(callbackFn)
+   groupToMap(callbackFn, thisArg)
 
-    // Inline callback function
-    groupToMap(function (element) { ... })
-    groupToMap(function (element, index) { ... })
-    groupToMap(function (element, index, array) { ... })
-    groupToMap(function (element, index, array) { ... }, thisArg)
+   // Inline callback function
+   groupToMap(function (element) { ... })
+   groupToMap(function (element, index) { ... })
+   groupToMap(function (element, index, array) { ... })
+   groupToMap(function (element, index, array) { ... }, thisArg)
  */
 
 
-    module.exports = {
-    
-    };
+module.exports = {
+
+};
