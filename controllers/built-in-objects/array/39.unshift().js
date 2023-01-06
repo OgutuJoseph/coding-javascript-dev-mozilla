b031b931c175605d12bcef05a6d78cbbe77f359f
OgutuJoseph <onyango.joseph@yahoo.com>
2023-01-06 15:12:57 +0300

    Built-in-objects--array-unshift()

--- controllers/built-in-objects/array/39.unshift().js
+++ controllers/built-in-objects/array/39.unshift().js
@@ -1,10 +1,12 @@
-/** ********** array methods - 39. toLocaleString() ********** */
+/** ********** array methods - 39. unshift() ********** */
 /**
     Syntax
-    toString()
+    unshift(element0)
+    unshift(element0, element1)
+    unshift(element0, element1, /.../ elementN)
 */
 
-/** **38.1 desc** */
+/** **39.1 desc** */
 const unshiftDesc = (req, res) => {
     // The unshift() method adds one or more elements to the beginning of an array and returns the new length of the array.
     
@@ -19,8 +21,30 @@ const unshiftDesc = (req, res) => {
     res.status(200).json(result);
 };
 
+/** **39.1 desc** */
+const unshiftOnNonArrays = (req, res) => {
+    const arrayLike = {
+        length: 3,
+        unrelated: "foo",
+        2: 4,
+    };
 
+    const arrayLike2 = {
+        length: 3,
+        unrelated: "foo",
+        2: 4,
+    };
+    Array.prototype.unshift.call(arrayLike2, 1, 2);
+
+    const result = {
+        arrayLike,
+        arrayLike2
+    };
+
+    res.status(200).json(result);
+};
 
 module.exports = {
-    
+    unshiftDesc,
+    unshiftOnNonArrays
 };
